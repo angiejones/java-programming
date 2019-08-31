98ffa8f9c5d8ae1e6f4c8764512fad36cc41d63f
Angie Jones <jones.angie@gmail.com>
2019-08-31 11:49:40 -0700

    Update Greetings.java
    closed scanner. thanks Srinivas Kadiyala for reporting the issue

--- chapter5/Greetings.java
+++ chapter5/Greetings.java
@@ -13,6 +13,7 @@ public class Greetings {
         System.out.println("Enter your name:");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
+        scanner.close();
         greetUser(name);
     }
 
